6bea6ba52e6ca2f596779e915256d4eaa8eacf43
SumedhChobitkar <158187477+SumedhChobitkar@users.noreply.github.com>
2026-02-18 14:57:05 +0530

    Add files via upload

---
+++ Mission-Shakti/src/main/java/com/Mission/Shakti/MissionShaktiApplication.java
@@ -0,0 +1,13 @@
+package com.Mission.Shakti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionShaktiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionShaktiApplication.class, args);
+	}
+
+}
---
+++ Mission-Shakti/src/test/java/com/Mission/Shakti/MissionShaktiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mission.Shakti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionShaktiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
